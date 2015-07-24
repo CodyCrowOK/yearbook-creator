7d765d62ce74c34cac6a07d0370ad6fa600df02a
Cody Crow <cody@cms07.org>
2015-07-24 14:04:51 -0500

    Finished splash screen

--- src/writer/Creator.java
+++ src/writer/Creator.java
@@ -2581,7 +2581,60 @@ public class Creator {
 				splash.close();
 				splash.dispose();
 				shell.open();
-				fileNewItem.getListeners(SWT.Selection)[0].handleEvent(event);
+				
+				final Shell dialog = new Shell(shell, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+				dialog.setText("Enter Yearbook Name");
+				dialog.setSize(400, 300);
+				FormLayout formLayout = new FormLayout();
+				formLayout.marginWidth = 10;
+				formLayout.marginHeight = 10;
+				formLayout.spacing = 10;
+				dialog.setLayout(formLayout);
+
+				Label label = new Label(dialog, SWT.NONE);
+				label.setText("New yearbook name:");
+				FormData data = new FormData();
+				label.setLayoutData(data);
+				
+				Button cancel = new Button(dialog, SWT.PUSH);
+				cancel.setText("Cancel");
+				data = new FormData();
+				data.width = 60;
+				data.right = new FormAttachment(100, 0);
+				data.bottom = new FormAttachment(100, 0);
+				cancel.setLayoutData(data);
+				cancel.setEnabled(false);
+
+				final Text text = new Text(dialog, SWT.BORDER);
+				data = new FormData();
+				data.width = 200;
+				data.left = new FormAttachment(label, 0, SWT.DEFAULT);
+				data.right = new FormAttachment(100, 0);
+				data.top = new FormAttachment(label, 0, SWT.CENTER);
+				data.bottom = new FormAttachment(cancel, 0, SWT.DEFAULT);
+				text.setLayoutData(data);
+
+				Button ok = new Button(dialog, SWT.PUSH);
+				ok.setText("OK");
+				data = new FormData();
+				data.width = 60;
+				data.right = new FormAttachment(cancel, 0, SWT.DEFAULT);
+				data.bottom = new FormAttachment(100, 0);
+				ok.setLayoutData(data);
+				ok.addSelectionListener(new SelectionAdapter() {
+					@Override
+					public void widgetSelected (SelectionEvent e) {
+						saveFileName = null;
+						createNewYearbook(text.getText());
+						dialog.close();
+						dialog.dispose();
+						refresh();
+					}
+				});
+
+				dialog.setDefaultButton (ok);
+				dialog.pack();
+				dialog.open();
 			}
 
 		});
