feab5e939dfce5c07bde6ee0963fb8a0025ff1d3
Cody Crow <cody@cms07.org>
2015-08-19 02:04:40 -0500

    v1.01

--- src/writer/Creator.java
+++ src/writer/Creator.java
@@ -59,7 +59,7 @@ import reader.ProductKey;
 public class Creator {
 
 	//Meta information
-	public static final String VERSION = "1.00";
+	public static final String VERSION = "1.01";
 	public static final String COMPANY_NAME = "Digital Express";
 	public static final String SOFTWARE_NAME = "Yearbook Designer";
 
